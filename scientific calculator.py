5ebec5f95d0671ceeae67cea4493bcd078e2c5ec
fathima mp <fathimamp37@gmail.com>
2024-06-28 21:42:29 +0530

    create menu bar

--- scientific calculator.py
+++ scientific calculator.py
@@ -437,3 +437,45 @@ lblDisplay = Label(calc, text="Scientific Calculator",
 				font=('Helvetica', 30, 'bold'),
 				bg='black', fg='white', justify=CENTER)
 lblDisplay.grid(row=0, column=4, columnspan=4)
+
+# use askyesno function to
+# stop/continue the program execution
+def iExit():
+	iExit = tkinter.messagebox.askyesno("Scientific Calculator",
+										"Do you want to exit ?")
+	if iExit>0:
+		root.destroy()
+		return
+
+def Scientific():
+	root.resizable(width=False, height=False)
+	root.geometry("944x568+0+0")
+
+
+def Standard():
+	root.resizable(width=False, height=False)
+	root.geometry("480x568+0+0")
+
+menubar = Menu(calc)
+
+# ManuBar 1 :
+
+filemenu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label = 'File', menu = filemenu)
+filemenu.add_command(label = "Standard", command = Standard)
+filemenu.add_command(label = "Scientific", command = Scientific)
+filemenu.add_separator()
+filemenu.add_command(label = "Exit", command = iExit)
+
+# ManuBar 2 :
+
+editmenu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label = 'Edit', menu = editmenu)
+editmenu.add_command(label = "Cut")
+editmenu.add_command(label = "Copy")
+editmenu.add_separator()
+editmenu.add_command(label = "Paste")
+
+root.config(menu=menubar)
+
+root.mainloop()
